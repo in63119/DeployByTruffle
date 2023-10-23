45750e5d2e9a0d3a52ae5bcd967fb617d32910ad
이인수 <in63119@gmail.com>
2023-10-23 11:27:32 +0900

    Fix: config

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,10 @@
 require("dotenv").config();
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 
-const { ADDRESS, PRIVATE_KEY, RPC_URL } = process.env;
+const { PRIVATE_KEY, RPC_URL } = process.env;
 
 module.exports = {
   networks: {
-    ganache: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*",
-    },
     klaytnTest: {
       provider: () => new HDWalletProvider(PRIVATE_KEY, RPC_URL),
       network_id: 1001,
